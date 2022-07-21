1a11a217fca77ea756b06a1b418521320bf146f6
RedRoboy <45677330+alexgornovoi@users.noreply.github.com>
2022-07-21 15:02:39 -0400

    function darkmode switch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import {React, useState} from 'react'
 import {ThemeProvider} from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import Themes from './lib/theme'
 import {Typography, Container} from '@mui/material'
 import LinkButton from './components/linkButton'
 import {LinkedIn, GitHub} from '@mui/icons-material'
+import LightSwitch from './components/themeSwitch'
 
 function App() {
+  const [mode, setMode] = useState(false);
+  const changeMode = () => {
+    setMode(!mode)
+  }
   return (
-    <ThemeProvider theme = {Themes.LightTheme}>
+    <ThemeProvider theme = {mode? Themes.DarkTheme : Themes.LightTheme}>
       <CssBaseline enableColorScheme/>
       <Container maxWidth = "sm" >
         <Typography variant="h2" align='center'>Welcome To my Website</Typography>
         <br/>
+        <LightSwitch m = {mode} func = {changeMode} />
         <LinkButton url='https://www.linkedin.com/in/alexeygornovoi/' text='LinkedIn' icon={<LinkedIn />} />
         <br/>
         <LinkButton url='https://github.com/alexgornovoi' text='GitHub' icon={<GitHub />} />
---
+++ src/components/themeSwitch.js
@@ -0,0 +1,10 @@
+import { IconButton } from "@mui/material";
+import { DarkMode, LightMode } from "@mui/icons-material";
+
+const LightSwitch = ({mode, func}) => {
+    return(
+        <IconButton onClick={func}>{{mode}? <LightMode />:<DarkMode />} </IconButton>
+    )
+}
+
+export default LightSwitch

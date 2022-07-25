d41968f15d54e7e57760fe8692c429b8b06ca4c4
RedRoboy <45677330+alexgornovoi@users.noreply.github.com>
2022-07-25 09:31:00 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import {React, useState, useEffect} from 'react'
-import {ThemeProvider} from '@mui/material/styles'
+import { React, useState, useEffect } from 'react'
+import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import Themes from './lib/theme'
-import {Typography, Container, AppBar, Toolbar} from '@mui/material'
+import { Typography, Container, AppBar, Toolbar } from '@mui/material'
 import LinkButton from './components/linkButton'
-import {LinkedIn, GitHub, Code, Web } from '@mui/icons-material'
+import { LinkedIn, GitHub, Code, Web } from '@mui/icons-material'
 import LightSwitch from './components/themeSwitch'
 
 function App() {
@@ -13,32 +13,34 @@ function App() {
   useEffect(() => {
     localStorage.setItem('theme-mode', mode);
   }, [mode]);
-  
+
   const changeMode = () => {
     setMode(!mode)
   }
 
 
   return (
-    <ThemeProvider theme = {mode? Themes.DarkTheme : Themes.LightTheme}>
-      <CssBaseline enableColorScheme/>
+    <ThemeProvider theme={mode ? Themes.DarkTheme : Themes.LightTheme}>
+      <CssBaseline enableColorScheme />
       <div>
-        <Container maxWidth = "md" >
-          <AppBar position='sticky'>
-            <Toolbar>
-              <Typography variant='h6' style={{ flex: 1 }}>Alex Links</Typography>
-              <LightSwitch mode = {mode} func = {changeMode}/>
-            </Toolbar>
-          </AppBar>
-          <Typography variant="h2">Hello</Typography>
-          <br/>
-          <LinkButton url='https://www.linkedin.com/in/alexeygornovoi/' text='LinkedIn' icon={<LinkedIn />} />
-          <br/>
-          <LinkButton url='https://github.com/alexgornovoi' text='GitHub' icon={<GitHub />} />
-          <br/>
-          <LinkButton url='https://leetcode.com/alexgornovoi/' text='LeetCode' icon = {<Code />} />
-          <br/>
-          <LinkButton url='https://alexgornovoi.com' text='Personnal Website' icon = {<Web />} off = {true} />
+        <Container maxWidth="md" >
+          <div>
+            <AppBar position='sticky'>
+              <Toolbar>
+                <Typography variant='h6' style={{ flex: 1 }}>Alex Links</Typography>
+                <LightSwitch mode={mode} func={changeMode} />
+              </Toolbar>
+            </AppBar>
+            <Typography variant="h2">Hello</Typography>
+            <br />
+            <LinkButton url='https://www.linkedin.com/in/alexeygornovoi/' text='LinkedIn' icon={<LinkedIn />} />
+            <br />
+            <LinkButton url='https://github.com/alexgornovoi' text='GitHub' icon={<GitHub />} />
+            <br />
+            <LinkButton url='https://leetcode.com/alexgornovoi/' text='LeetCode' icon={<Code />} />
+            <br />
+            <LinkButton url='https://alexgornovoi.com' text='Personnal Website' icon={<Web />} off={true} />
+          </div>
         </Container>
       </div>
     </ThemeProvider>

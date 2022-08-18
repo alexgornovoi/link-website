0d67a8cc2326052c05e03c95489abd93b92f4100
RedRoboy <45677330+alexgornovoi@users.noreply.github.com>
2022-08-18 12:44:16 -0400

    Update buttons.js

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -8,7 +8,7 @@ const Buttons = ({ mode }) => {
             <LinkButton text="LinkedIn" icon={<LinkedIn />} url="https://www.linkedin.com/in/alexeygornovoi/" />
             <LinkButton text="GitHub" icon={<GitHub />} url="https://github.com/alexgornovoi" />
             <LinkButton text="LeetCode" icon={<Code />} url="https://leetcode.com/alexgornovoi/" />
-            <LinkButton text="Website" icon={<Web />} url="https://alexgornovoi.github.io/link-website/" />
+            <LinkButton text="Website" icon={<Web />} url="https://www.alexgornovoi.com/" />
             <LinkButton text="Source" icon={<Javascript />} url="https://github.com/alexgornovoi/link-website" />
         </ButtonGroup>
     )
